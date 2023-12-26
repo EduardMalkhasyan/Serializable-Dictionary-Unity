113f11e07168a2a6f4a59856d38dfd1cdcd31690
Eduard Malkhasyan <78969017+EduardMalkhasyan@users.noreply.github.com>
2023-12-26 19:17:41 +0400

    serialized dict work

--- Assets/SerializableDictionary/Scripts/Player.cs
+++ Assets/SerializableDictionary/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using ProjectTools;
 
 public class Player : MonoBehaviour
 {
--- Assets/SerializableDictionary/Scripts/SerializableDictionary.cs
+++ Assets/SerializableDictionary/Scripts/SerializableDictionary.cs
@@ -6,114 +6,117 @@ using System.Linq;
 using UnityEditor;
 #endif
 
-[Serializable]
-public class SerializableDictionary<TKey, TValue> : ISerializationCallbackReceiver
+namespace ProjectTools
 {
-    [HelpBox(HelpBoxMessageType.Error)]
-    [SerializeField] private List<KeyValueEntry> entries;
-    private List<TKey> keys = new List<TKey>();
-
-    public Dictionary<TKey, TValue> dictionary = new Dictionary<TKey, TValue>();
-
     [Serializable]
-    class KeyValueEntry
+    public class SerializableDictionary<TKey, TValue> : ISerializationCallbackReceiver
     {
-        public TKey key;
-        public TValue value;
+        [SerializeField, SDHelpBox] private List<KeyValueEntry> entries;
+        private List<TKey> keys = new List<TKey>();
+
+        public Dictionary<TKey, TValue> dictionary = new Dictionary<TKey, TValue>();
+
+        [Serializable]
+        class KeyValueEntry
+        {
+            public TKey key;
+            public TValue value;
+        }
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            dictionary.Clear();
+
+            for (int i = 0; i < entries.Count; i++)
+            {
+                dictionary.TryAdd(entries[i].key, entries[i].value);
+            }
+        }
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+            if (entries == null)
+            {
+                return;
+            }
+
+            keys.Clear();
+
+            for (int i = 0; i < entries.Count; i++)
+            {
+                keys.Add(entries[i].key);
+            }
+
+            var result = keys.GroupBy(x => x)
+                             .Where(g => g.Count() > 1)
+                             .Select(x => new { Element = x.Key, Count = x.Count() })
+                             .ToList();
+
+            if (result.Count > 0)
+            {
+                var duplicates = string.Join(", ", result);
+                var errorMessage = $"Warning founded duplicates in: {duplicates}";
+                SDMessage.errorText = errorMessage;
+                SDMessage.showIf = true;
+            }
+            else
+            {
+                SDMessage.showIf = false;
+            }
+        }
     }
 
-    void ISerializationCallbackReceiver.OnAfterDeserialize()
-    {
-        dictionary.Clear();
-
-        for (int i = 0; i < entries.Count; i++)
-        {
-            dictionary.TryAdd(entries[i].key, entries[i].value);
-        }
-    }
-
-    void ISerializationCallbackReceiver.OnBeforeSerialize()
-    {
-        if (entries == null)
-        {
-            return;
-        }
-
-        keys.Clear();
-
-        for (int i = 0; i < entries.Count; i++)
-        {
-            keys.Add(entries[i].key);
-        }
-
-        var result = keys.GroupBy(x => x)
-                         .Where(g => g.Count() > 1)
-                         .Select(x => new { Element = x.Key, Count = x.Count() })
-                         .ToList();
-
-        if (result.Count > 0)
-        {
-            var duplicates = string.Join(", ", result);
-            var errorMessage = $"Warning {GetType().Name} keys has duplicates {duplicates}";
-            Message.errorText = errorMessage;
-            Message.showIf = true;
-        }
-        else
-        {
-            Message.showIf = false;
-        }
-    }
-
-}
-
+    #region Editor
 #if UNITY_EDITOR
-public enum HelpBoxMessageType { None, Info, Warning, Error }
+    public enum SDHelpBoxMessageType { None, Info, Warning, Error }
 
-public static class Message
-{
-    public static string errorText;
-    public static bool showIf;
-}
-
-public class HelpBoxAttribute : PropertyAttribute
-{
-    public HelpBoxMessageType messageType;
-
-    public HelpBoxAttribute(HelpBoxMessageType messageType = HelpBoxMessageType.None)
+    public static class SDMessage
     {
-        this.messageType = messageType;
-    }
-}
-
-[CustomPropertyDrawer(typeof(HelpBoxAttribute))]
-public class HelpBoxAttributeDrawer : DecoratorDrawer
-{
-    public override float GetHeight()
-    {
-        var helpBoxAttribute = attribute as HelpBoxAttribute;
-        if (helpBoxAttribute == null || !Message.showIf) return base.GetHeight();
-        var helpBoxStyle = (GUI.skin != null) ? GUI.skin.GetStyle("helpbox") : null;
-        if (helpBoxStyle == null) return base.GetHeight();
-        return Mathf.Max(40f, helpBoxStyle.CalcHeight(new GUIContent(Message.errorText), EditorGUIUtility.currentViewWidth) + 4);
+        public static string errorText;
+        public static bool showIf;
     }
 
-    public override void OnGUI(Rect position)
+    public class SDHelpBoxAttribute : PropertyAttribute
     {
-        var helpBoxAttribute = attribute as HelpBoxAttribute;
-        if (helpBoxAttribute == null || !Message.showIf) return;
-        EditorGUI.HelpBox(position, Message.errorText, GetMessageType(helpBoxAttribute.messageType));
-    }
+        public SDHelpBoxMessageType messageType;
 
-    private MessageType GetMessageType(HelpBoxMessageType helpBoxMessageType)
-    {
-        switch (helpBoxMessageType)
+        public SDHelpBoxAttribute(SDHelpBoxMessageType messageType = SDHelpBoxMessageType.Error)
         {
-            default:
-            case HelpBoxMessageType.None: return MessageType.None;
-            case HelpBoxMessageType.Info: return MessageType.Info;
-            case HelpBoxMessageType.Warning: return MessageType.Warning;
-            case HelpBoxMessageType.Error: return MessageType.Error;
+            this.messageType = messageType;
         }
     }
-}
+
+    [CustomPropertyDrawer(typeof(SDHelpBoxAttribute))]
+    public class SDHelpBoxAttributeDrawer : DecoratorDrawer
+    {
+        public override float GetHeight()
+        {
+            var helpBoxAttribute = attribute as SDHelpBoxAttribute;
+            if (helpBoxAttribute == null || SDMessage.showIf == false) return base.GetHeight();
+            var helpBoxStyle = (GUI.skin != null) ? GUI.skin.GetStyle("helpbox") : null;
+            if (helpBoxStyle == null) return base.GetHeight();
+            return Mathf.Max(40f, helpBoxStyle.CalcHeight(new GUIContent(SDMessage.errorText), EditorGUIUtility.currentViewWidth) + 4);
+        }
+
+        public override void OnGUI(Rect position)
+        {
+            var helpBoxAttribute = attribute as SDHelpBoxAttribute;
+            if (helpBoxAttribute == null || SDMessage.showIf == false) return;
+            EditorGUI.HelpBox(position, SDMessage.errorText, GetMessageType(helpBoxAttribute.messageType));
+        }
+
+        private MessageType GetMessageType(SDHelpBoxMessageType helpBoxMessageType)
+        {
+            switch (helpBoxMessageType)
+            {
+                default:
+                case SDHelpBoxMessageType.None: return MessageType.None;
+                case SDHelpBoxMessageType.Info: return MessageType.Info;
+                case SDHelpBoxMessageType.Warning: return MessageType.Warning;
+                case SDHelpBoxMessageType.Error: return MessageType.Error;
+            }
+        }
+    }
 #endif
+    #endregion
+}
